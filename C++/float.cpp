9efccd872ecceb7c9bce1d4384a7aad0eb10cbcd
Aditya <kraditya9241@gmail.com>
2025-08-13 16:42:28 +0530

    update

---
+++ C++/float.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float x = 5 , y = 2; // declare and initialize x and y
+    cout << x + y << endl; // output the sum of x and y
+    cout << x - y << endl; // output the difference of x and y 
+    cout << x << endl << y << endl; // output the values of x and y 
+    return 0; // end of the program
+}
--- C++/upadtionofvariable.cpp
+++ C++/upadtionofvariable.cpp
@@ -1,10 +1,21 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int x = 5; // declare and initialize x
+//     cout << x<< endl; // output the value of x
+//     x = 8; // change the value of x
+//     cout << x << endl; // output the new value of x
+    
+//     return 0;
+// }
+
+#include <iostream>
 using namespace std;
 int main() {
-    int x = 5; // declare and initialize x
-    cout << x<< endl; // output the value of x
-    x = 8; // change the value of x
-    cout << x << endl; // output the new value of x
-    
+    int x=  7, y = 3; // declare and initialize x and y
+    cout << x+y<< endl;
+    cout << x-y<< endl;
+    cout << x*y<< endl;
+    cout << x/y<< endl; // output the value of x divided by y
     return 0;
 }
