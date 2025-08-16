0d92bb2cb5348bd0c386daa06d4264bc9d72feee
Aditya <kraditya9241@gmail.com>
2025-08-16 22:33:19 +0530

    update

--- C++/FUNCTION-CH-5/pascal-triangel.cpp
+++ C++/FUNCTION-CH-5/pascal-triangel.cpp
@@ -2,3 +2,31 @@
 
 using namespace std;
 
+int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int ncr(int n, int r) {
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+int main () {
+    int n;
+    cout << "Enter the number of rows for Pascal's Triangle: ";
+    cin >> n;
+    for (int i= 0; i<=n; i++) {
+         for (int j = 0; j <= n-i+1; j++) {
+            cout  << "  ";
+        } 
+        for (int j = 0; j <= i; j++) {
+            cout << ncr(i, j) << "   ";
+        }   
+        cout << endl;
+    }
+    return 0;
+
+}
