5c850e5e0e717bde1fefb89fc5e0ba241bdfb041
Aditya <kraditya9241@gmail.com>
2025-08-17 16:56:11 +0530

    update

--- C++/FUNCTION-CH-5/swap-number.cpp
+++ C++/FUNCTION-CH-5/swap-number.cpp
@@ -2,3 +2,13 @@
 
 using namespace std;
 
+int main () {
+    int x = 2;
+    int y= 5;
+    cout << "Before swapping: x = " << x << ", y = " << y << endl;
+    x = x + y; // Step 1: Add x and y
+    y = x - y; // Step 2: Subtract y from the result of Step
+    x = x - y; // Step 3: Subtract y from the result of Step
+    cout << "After swapping: x = " << x << ", y = " << y << endl;
+    return 0;
+}
