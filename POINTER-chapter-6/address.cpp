f1995291da92382dddf055ee084842da7a3ce277
Aditya <kraditya9241@gmail.com>
2025-08-17 17:44:30 +0530

    update

---
+++ POINTER-chapter-6/address.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x = 3;
+    cout << "Address of x: " << &x << endl;
+    int y= 3;
+    cout << "Address of y: " << &y << endl;
+}
