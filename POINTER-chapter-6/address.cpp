0f17ca4421f874be9bc81a593ed8ff386be07e2e
Aditya <kraditya9241@gmail.com>
2025-08-17 18:16:04 +0530

    update

--- POINTER-chapter-6/address.cpp
+++ POINTER-chapter-6/address.cpp
@@ -6,4 +6,9 @@ int main () {
     cout << "Address of x: " << &x << endl;
     int y= 3;
     cout << "Address of y: " << &y << endl;
+    cout << x<< endl;
+    int *p;
+    p = &x;
+    cout << "Address of x: " << p << endl;
+    cout << "Value of x: " << *p << endl;   
 }
