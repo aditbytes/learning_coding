8c02b87385dc6abf22777547fe9586636f9ea6c1
Aditya <kraditya9241@gmail.com>
2025-08-18 20:14:27 +0530

    update

--- ARRAYS/syntaxArrays.cpp
+++ ARRAYS/syntaxArrays.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 int main() {
     int arr[5]; // Declare an array of size 5
-    arr[1] = 10; // Assign value to the second element
-    arr[3] = 20; // Assign value to the fourth element  
-    arr[4] = 30; // Assign value to the fifth element
-    arr[0] = 5;  // Assign value to the first element
-    arr[2] = 40; // Assign value to the third element
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i]; // Input elements into the array
+    }
 
     for (int i = 0; i < 5; i++) {
         cout << "Element at index " << i << ": " << arr[i] << endl; // Print each element
