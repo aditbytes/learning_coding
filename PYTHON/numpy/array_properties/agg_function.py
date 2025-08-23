f521e45f754e737740442160b3f5db99b0355756
Aditya <kraditya9241@gmail.com>
2025-08-23 20:54:26 +0530

    update 23-08

---
+++ PYTHON/numpy/array_properties/agg_function.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+arr = np.array([10,20,30,40,50,60,70,80,90,100])
+
+print(np.sum(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.mean(arr))
+print(np.median(arr))
+print(np.std(arr))
+print(np.var(arr))
+print(np.argmin(arr))
+print(np.argmax(arr))
+print(np.cumsum(arr))
+print(np.cumprod(arr))
---
+++ PYTHON/numpy/array_properties/astype.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+
+arr = np.array([1.2,2.5,3])
+int_arr = arr.astype('int')
+
+print(arr)
+print(int_arr)
+print(arr.dtype)
+print(int_arr.dtype)
