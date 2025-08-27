8738d5983fd637e2d3eed7b1f47a8d6e1d448fe1
Aditya <kraditya9241@gmail.com>
2025-08-27 16:14:13 +0530

    update

---
+++ C++/STL/list.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+int main () {
+    list<int> l;
+    l.push_back(10);
+    l.push_back(20);
+    l.push_back(30);
+    l.push_back(40);
+    l.push_back(50);
+
+    for (int val : l ) {
+        cout << val << " ";
+    }
+    
+
+    cout << endl;
+    return 0;
+
+
+
+    
+}
