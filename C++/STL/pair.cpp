6aed0b0f57b2b9600aa221cc886d3bfe0df3bee5
Aditya <kraditya9241@gmail.com>
2025-09-01 17:53:42 +0530

    update

---
+++ C++/STL/pair.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+
+
+using namespace std;
+
+int main () {
+    pair<int, int> p = {3,5};
+
+    cout <<p.first<<endl;
+    cout <<p.second<<endl;
+    return 0;
+}
