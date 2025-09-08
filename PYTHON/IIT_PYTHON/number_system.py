164216c2de1ff9f74a579ab285d10d25843e0516
Aditya <kraditya9241@gmail.com>
2025-09-08 20:46:41 +0530

    update and remove exe file for c++

--- PYTHON/IIT_PYTHON/number_system.py
+++ PYTHON/IIT_PYTHON/number_system.py
@@ -1,1 +1,17 @@
-number = input("Enter a number: ")
+num = input("Enter a number: ")
+
+print ("/n conversions:")
+
+print (f"Binary: {bin(int(num))}")
+print (f"Octal: {oct(int(num))}")
+print (f"Hexadecimal: {hex(int(num))}")
+print (f"Decimal: {int(num)}")
+print (f"Character: {chr(int(num))}")
+print (f"Float: {float(num)}")  
+print (f"Complex: {complex(num)}")
+print (f"Boolean: {bool(int(num))}")
+
+print (f"binary : {bin(int(num))[2:]}")
+print (f"octal : {oct(int(num))[2:]}")
+print (f"hexadecimal : {hex(int(num))[2:]}")
+print (f"decimal : {int(num)}")
