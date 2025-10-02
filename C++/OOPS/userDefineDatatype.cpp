05b37bbf9a51edd0d7c12cc74755e76ec7f97ba7
Aditya . <kraditya9241@gmail.com>
2025-10-02 20:10:48 +0530

    update

---
+++ C++/OOPS/userDefineDatatype.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class student {
+public:
+    string name;
+    int roll;
+};
+int main(){
+    student s;
+    s.name = "aditya";
+    s.roll = 123;
+    cout<<s.name<<endl;
+    cout<<s.roll<<endl;
+    return 0;
+
+}
